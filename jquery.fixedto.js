a939ce79d97aacc8185161cb8b518188aa628c25
Nelly Natalí <unanatali@gmail.com>
2011-07-09 23:27:34 +0200

    1.1 ready!

--- jquery.fixedto.js
+++ jquery.fixedto.js
@@ -2,14 +2,14 @@
  * jQuery fixedTo
  *
  * Revision: 1.1
- * Date: 06/06/2011
+ * Date: 09/06/2011
  *
  * Author: Nelly Natali (@nnatali)
- * Plugin URL: http://experimentos.nnatali.com/jquery/plugin-fixedto/
+ * Plugin URL: https://github.com/nnatali/jquery.fixedTo
  * License: http://creativecommons.org/licenses/by-sa/3.0/
  *
 */
-(function(jQuery) {
+(function(jQuery){
 	
   jQuery.fn.fixedTo = function(options_user){
 	  
@@ -20,7 +20,7 @@
 				align: "center",
 				hide: true,
 				height_hide: 15
-			};
+			}
 			options = jQuery.extend(options_default,options_user);
 			position = options.position;
 			align = options.align;
@@ -30,59 +30,93 @@
 			var element = jQuery(this),
 					height_element = element.outerHeight(),
 					width_element = element.outerWidth();
+
+			isIE = jQuery.browser.msie ? true : false;
+
+			element.css({"position":"fixed"});
 			
 			position_calculate = function(){
 				
-		 		thewindow = jQuery(window),
+				thewindow = jQuery(window),
 				height_thewindow = thewindow.height(),
 				width_thewindow = thewindow.width();
 				
-				if(position == "top" || position == "bottom") {
+				if(position=="top" || position=="bottom"){
+					
 					var vertical = position == "top" ? "top" : "bottom",
 							horizontal = align == "right" ? "right" : "left",
 							margin_horizontal = align == "center" ? (width_thewindow - width_element) / 2 : 0;
+							
 					var new_style = {};
 							new_style[vertical] = 0,
 							new_style[horizontal] = margin_horizontal+"px";
-					element.addClass("fixedTo");
+					
 				}
 				
-				if (position == "right") {
-					if(align == "left")
+				if (position=="right"){
+					
+					if(align=="left"){
 						y = 0;
-					else if (align == "center")
+					}else if(align=="center"){
 						y = (height_thewindow - width_element) / 2;
-					else if (align == "right")
+					}else if(align=="right"){
 						y = height_thewindow - width_element;
-					x = ((width_thewindow - width_element) / 2) + width_element;
+					}
+						
+					x = (width_thewindow - width_element) + width_element;
+					
 					var new_style = {};
-							new_style["-ms-filter"] = "progid:DXImageTransform.Microsoft.Matrix(M11=-0, M12=-1, M21=1, M22=-0,sizingMethod='auto expand')",
-							new_style["filter"] = "progid:DXImageTransform.Microsoft.Matrix(M11=0, M12=-1, M21=1, M22=0,sizingMethod='auto expand')",
 							new_style["-moz-transform-origin"] = "0 0",
 							new_style["-moz-transform"] = "rotate(90deg) translate("+Number(y)+""+"px,-"+Number(x)+"px)",
 							new_style["-webkit-transform-origin"] = "0 0",
 							new_style["-webkit-transform"] = "rotate(90deg) translate("+Number(y)+""+"px,-"+Number(x)+"px)",
+							new_style["ms-transform-origin"] = "0 0",
+							new_style["ms-transform"] = "rotate(90deg) translate("+Number(y)+""+"px,-"+Number(x)+"px)",
 							new_style["transform-origin"] = "0 0",
 							new_style["transform"] = "rotate(90deg) translate("+Number(y)+""+"px,-"+Number(x)+"px)";
+							
+					if(isIE && jQuery.browser.version!="9.0"){
+							new_style["-ms-filter"] = "progid:DXImageTransform.Microsoft.Matrix(M11=-0, M12=-1, M21=1, M22=-0,sizingMethod='auto expand')";
+							new_style["filter"] = "progid:DXImageTransform.Microsoft.Matrix(M11=0, M12=-1, M21=1, M22=0,sizingMethod='auto expand')";
+							x = width_thewindow - height_element;
+							new_style["marginTop"] = Number(y)+"px";
+							new_style["marginLeft"] = Number(x)+"px";
+							if(jQuery.browser.version=="7.0"){ new_style["right"] = 0; }
+					}
 				}
 				
-				if (position == "left") {
-					if(align == "right")
+				if(position=="left"){
+					
+					if(align=="right"){
 						y = width_element;
-					else if (align == "center")
+					}else if(align=="center"){
 						y = width_element + ((height_thewindow - width_element) / 2);
-					else if (align == "left")
+					}else if(align=="left"){
 						y = width_element + (height_thewindow - width_element);
-					x = (width_thewindow - width_element) / 2;
+					}
+					
+					x = 0;
+					
 					var new_style = {};
-							new_style["-ms-filter"] = "progid:DXImageTransform.Microsoft.Matrix(M11=-0, M12=1, M21=-1, M22=-0,sizingMethod='auto expand')",
-							new_style["filter"] = "progid:DXImageTransform.Microsoft.Matrix(M11=-0, M12=1, M21=-1, M22=-0,sizingMethod='auto expand')",
 							new_style["-moz-transform-origin"] = "0 0",
 							new_style["-moz-transform"] = "rotate(270deg) translate(-"+Number(y)+""+"px,-"+Number(x)+"px)",
 							new_style["-webkit-transform-origin"] = "0 0",
 							new_style["-webkit-transform"] = "rotate(270deg) translate(-"+Number(y)+""+"px,-"+Number(x)+"px)",
+							new_style["ms-transform-origin"] = "0 0",
+							new_style["ms-transform"] = "rotate(270deg) translate(-"+Number(y)+""+"px,-"+Number(x)+"px)",
 							new_style["transform-origin"] = "0 0",
 							new_style["transform"] = "rotate(270deg) translate(-"+Number(y)+""+"px,-"+Number(x)+"px)";
+							
+					if(isIE && jQuery.browser.version!="9.0"){
+							new_style["-ms-filter"] = "progid:DXImageTransform.Microsoft.Matrix(M11=-0, M12=1, M21=-1, M22=-0,sizingMethod='auto expand')";
+							new_style["filter"] = "progid:DXImageTransform.Microsoft.Matrix(M11=-0, M12=1, M21=-1, M22=-0,sizingMethod='auto expand')";
+							if(align == "left"){ y = height_thewindow - width_element; }
+							if(align == "center"){ y = (height_thewindow - width_element) / 2; }
+							if(align == "right"){ y = "0"; }
+							new_style["marginTop"] = Number(y)+"px";
+							new_style["marginLeft"] = 0;
+							if(jQuery.browser.version=="7.0"){ new_style["left"] = 0; }
+					}
 				}
 				
 				element.css(new_style);
@@ -92,20 +126,26 @@
 			show_hide = function(){
 				
 				element.prepend('<a href="#" class="close">X</a>');
-				jQuery('.close', element).toggle(function() {
-					element.css({"overflow":"hidden"}).animate({"height":height_hide+"px"});
+				
+				jQuery('.close', element).toggle(function(){
+					if (isIE) element.css({"overflow":"hidden","height":height_hide+"px"});
+					else element.css({"overflow":"hidden"}).animate({"height":height_hide+"px"});
 			  }, function(){
-				  element.css({"overflow":"auto"}).animate({"height":height_element+"px"});
+				  if (isIE) element.css({"overflow":"auto","height":"auto"});
+					else element.css({"overflow":"auto"}).animate({"height":height_element+"px"});
 				});
 				
 			}
 			
 			position_calculate();
+
 			jQuery(window).resize(function(){
 				position_calculate();
 			});
 
-			if (hide === true) show_hide();
+			if(hide === true){
+				show_hide();
+			}
 			
     });
 	  
